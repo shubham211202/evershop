6e467780e14e52401a9f6f03a7f4205071219d61
shubham211202 <jangidshubham211202@gmail.com>
2025-09-19 08:53:37 +0000

    update

--- .evershop/build/frontStore/checkout/client/entry.js
+++ .evershop/build/frontStore/checkout/client/entry.js
@@ -4,29 +4,29 @@
       import { Area } from '@evershop/evershop/components/common';
       import {HydrateFrontStore} from '@evershop/evershop/components/common';
       
-import e9ecf5ef8543fb207f1817d4796ae1884 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/catalog/pages/frontStore/all/SearchBox.js';
-import e17602fc1069e0057bfd8ebcb2c75a6bd from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/all/MiniCart.js';
-import e4fc9c310d1a6bff9d9460b97e3916a5f from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/Checkout.js';
-import e7cc504dcef8e240657405e23efcb2cdd from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/PaymentStep.js';
-import e327abae94395a517916a294eb3d9aa5d from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShipmentStep.js';
-import e97c74585ceecb98419fa44d9e7e24e0b from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShippingMethods.js';
-import ee65205c938c602c374f586150c41e72a from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShippingNote.js';
-import edc7a1723b97b5388125d49fc4e2f0122 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/Summary.js';
-import e7245c24cef9e6bd93f0622a416cf45e2 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/SummaryMobile.js';
-import eb52408d2e7f153ee66cbe515c679520b from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Breadcrumb.js';
-import ee4f06e0e9b0a702b40ab3d761e0305c1 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Footer.js';
-import ea14469e70a79392d6c86c8a22e53964a from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/HeadTags.js';
-import e9d94f064257cd97755d5f9920b570bfd from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Layout.js';
-import e49357aa605ce93a8ce1206553d0a2b82 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Logo.js';
-import e80fd16c408e659500665291caff6892f from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Notification.js';
-import e6b1602afeb7a972bc9d4d3ed55f7355f from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/cod/pages/frontStore/checkout/CashOnDelivery.js';
-import e37d9c56ea2df2993de9c2ad6bef05044 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/customer/pages/frontStore/all/UserIcon.js';
-import edd0ce66f66e209671eed32616c963e45 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/customer/pages/frontStore/checkout/CustomerInfoStep.js';
-import e9ba351aac9216fa59d67102a72cdd39a from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/paypal/pages/frontStore/checkout/Paypal.js';
-import ea6e85043f36fa3b75f075d586c84b7f6 from 'file:///root/evershop/node_modules/@evershop/evershop/dist/modules/stripe/pages/frontStore/checkout/Stripe.js';
-import collection_products from 'file:///root/evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/CollectionProducts.js';
-import text_block from 'file:///root/evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/TextBlock.js';
-import basic_menu from 'file:///root/evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/BasicMenu.js';
+import e9ecf5ef8543fb207f1817d4796ae1884 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/catalog/pages/frontStore/all/SearchBox.js';
+import e17602fc1069e0057bfd8ebcb2c75a6bd from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/all/MiniCart.js';
+import e4fc9c310d1a6bff9d9460b97e3916a5f from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/Checkout.js';
+import e7cc504dcef8e240657405e23efcb2cdd from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/PaymentStep.js';
+import e327abae94395a517916a294eb3d9aa5d from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShipmentStep.js';
+import e97c74585ceecb98419fa44d9e7e24e0b from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShippingMethods.js';
+import ee65205c938c602c374f586150c41e72a from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/ShippingNote.js';
+import edc7a1723b97b5388125d49fc4e2f0122 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/Summary.js';
+import e7245c24cef9e6bd93f0622a416cf45e2 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/checkout/pages/frontStore/checkout/SummaryMobile.js';
+import eb52408d2e7f153ee66cbe515c679520b from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Breadcrumb.js';
+import ee4f06e0e9b0a702b40ab3d761e0305c1 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Footer.js';
+import ea14469e70a79392d6c86c8a22e53964a from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/HeadTags.js';
+import e9d94f064257cd97755d5f9920b570bfd from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Layout.js';
+import e49357aa605ce93a8ce1206553d0a2b82 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Logo.js';
+import e80fd16c408e659500665291caff6892f from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cms/pages/frontStore/all/Notification.js';
+import e6b1602afeb7a972bc9d4d3ed55f7355f from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/cod/pages/frontStore/checkout/CashOnDelivery.js';
+import e37d9c56ea2df2993de9c2ad6bef05044 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/customer/pages/frontStore/all/UserIcon.js';
+import edd0ce66f66e209671eed32616c963e45 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/customer/pages/frontStore/checkout/CustomerInfoStep.js';
+import e9ba351aac9216fa59d67102a72cdd39a from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/paypal/pages/frontStore/checkout/Paypal.js';
+import ea6e85043f36fa3b75f075d586c84b7f6 from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/modules/stripe/pages/frontStore/checkout/Stripe.js';
+import collection_products from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/CollectionProducts.js';
+import text_block from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/TextBlock.js';
+import basic_menu from 'file:///root/my-evershop/node_modules/@evershop/evershop/dist/components/frontStore/widgets/BasicMenu.js';
 Area.defaultProps.components = {
   'icon-wrapper': {
     e9ecf5ef8543fb207f1817d4796ae1884: {
